d36c298e756b0726efdc8899f8773377339b9a39
LeMayday <21mayc@gmail.com>
2023-08-20 21:55:17 -0400

    added redirected cmd output

--- OSC_preprocessing_multinight_nodark_samebias.py
+++ OSC_preprocessing_multinight_nodark_samebias.py
@@ -3,6 +3,7 @@
 # Date: 8/20/2023
 # starter code from https://siril.org/tutorials/pysiril/
 
+import sys
 import os
 import tkinter as tk
 from tkinter import filedialog
@@ -70,55 +71,57 @@ def stack_lights(input_dir, output_dir):
 # create Siril instance
 app = Siril()
 
-try:
-    # folder chooser
-    # how to keep filedialog on top: https://stackoverflow.com/a/74643784
-    # https://blog.filestack.com/thoughts-and-knowledge/python-file-picker-best-practices/
-    window = tk.Tk()
-    window.withdraw()
-    window.attributes('-topmost', True)
+# folder chooser
+# how to keep filedialog on top: https://stackoverflow.com/a/74643784
+# https://blog.filestack.com/thoughts-and-knowledge/python-file-picker-best-practices/
+window = tk.Tk()
+window.withdraw()
+window.attributes('-topmost', True)
+workdir = filedialog.askdirectory(mustexist=True, parent=window, title="Select Work Directory")
+while not workdir:
     workdir = filedialog.askdirectory(mustexist=True, parent=window, title="Select Work Directory")
-    while not workdir:
-        workdir = filedialog.askdirectory(mustexist=True, parent=window, title="Select Work Directory")
 
-    # start wrapper, Siril, and set preferences
-    siril = Wrapper(app)
-    app.Open()
-    siril.set16bits()
-    siril.setext('fit')
+# https://stackoverflow.com/questions/4675728/redirect-stdout-to-a-file-in-python
+with open(workdir + '/log.txt', 'w') as sys.stdout:
+    try:
+        # start wrapper, Siril, and set preferences
+        siril = Wrapper(app)
+        app.Open()
+        siril.set16bits()
+        siril.setext('fit')
 
-    # set folder structure
-    process_dir = workdir + '/process'
-    # bias_process_dir is the output directory for converted biases, not for input
-    bias_process_dir = process_dir + '/biases'
-    session_dirs = []
+        # set folder structure
+        process_dir = workdir + '/process'
+        # bias_process_dir is the output directory for converted biases, not for input
+        bias_process_dir = process_dir + '/biases'
+        session_dirs = []
 
-    # https://stackoverflow.com/questions/141291/how-to-list-only-top-level-directories-in-python
-    for folder in next(os.walk(os.path.join(workdir, '.')))[1]:
-        if folder[0] == 'S' and folder[1:2].isdigit():
-            session_dirs.append(folder)
+        # https://stackoverflow.com/questions/141291/how-to-list-only-top-level-directories-in-python
+        for folder in next(os.walk(os.path.join(workdir, '.')))[1]:
+            if folder[0] == 'S' and folder[1:2].isdigit():
+                session_dirs.append(folder)
 
-    # prepare master bias
-    master_bias(workdir + '/biases', bias_process_dir)
+        # prepare master bias
+        master_bias(workdir + '/biases', bias_process_dir)
 
-    # prepare master flats and calibrate light frames for each session
-    light_seqs = []
-    for session in session_dirs:
-        master_flat(workdir + '/' + session + '/flats', process_dir + '/'
-                    + session, bias_process_dir)
-        calibrate_lights(workdir + '/' + session + '/lights', process_dir + '/'
+        # prepare master flats and calibrate light frames for each session
+        light_seqs = []
+        for session in session_dirs:
+            master_flat(workdir + '/' + session + '/flats', process_dir + '/'
                         + session, bias_process_dir)
-        light_seqs.append(process_dir + '/' + session + '/pp_light')
-    
-    # merge documentation says that the final item is the output sequence
-    light_seqs.append(process_dir + '/pp_light_group')
-    siril.merge(list_seq=light_seqs)
-    stack_lights(process_dir, workdir)
+            calibrate_lights(workdir + '/' + session + '/lights', process_dir + '/'
+                            + session, bias_process_dir)
+            light_seqs.append(process_dir + '/' + session + '/pp_light')
+        
+        # merge documentation says that the final item is the output sequence
+        light_seqs.append(process_dir + '/pp_light_group')
+        siril.merge(list_seq=light_seqs)
+        stack_lights(process_dir, workdir)
 
-except Exception as e:
-    print("\n**** ERROR *** " +  str(e) + "\n" )    
+    except Exception as e:
+        print("\n**** ERROR *** " +  str(e) + "\n" )    
 
-# close Siril and delete Siril instance
-app.Close()
-del app
-print("Done.")
+    # close Siril and delete Siril instance
+    app.Close()
+    del app
+    print("Done.")
