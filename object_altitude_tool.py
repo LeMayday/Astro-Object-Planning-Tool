dd9640b7d2a2c1de2beede48275aefeb01be1167
LeMayday <21mayc@gmail.com>
2024-06-29 22:46:25 -0700

    simple variable renaming

--- object_altitude_tool.py
+++ object_altitude_tool.py
@@ -17,11 +17,11 @@ def night_half_duration(local_midnight, location):
     sunset_to_midnight = (n - np.argmin(np.abs(sun.transform_to(local_frame).alt.degree))) / n * 13
     return sunset_to_midnight
 
-object = input("Enter the name of the object: ")
-object_coords = SkyCoord.from_name(object)
+celestial_object = input("Enter the name of the object: ")
+celestial_object_coords = SkyCoord.from_name(celestial_object)
 
-coords = input("Enter your latitude and longitude in degrees (separated by a space): ")
-lat, long = coords.split()
+local_coords = input("Enter your latitude and longitude in degrees (separated by a space): ")
+lat, long = local_coords.split()
 location = EarthLocation(lat=float(lat) * u.deg, lon=float(long) * u.deg, height = 0 * u.m)
 
 date = input("Enter the date of your observation (mm/dd/yyyy): ")
@@ -31,19 +31,17 @@ local_midnight = Time(timestamp.to_value('jd', 'float') + 1 - float(long) / 360,
 
 night_half_length = night_half_duration(local_midnight, location)
 delta_midnight = np.linspace(-night_half_length, night_half_length, 1000) * u.hour
-night = local_midnight + delta_midnight
-local_frame = AltAz(obstime=night, location=location)
+local_frame = AltAz(obstime=local_midnight + delta_midnight, location=location)
 
-object_alt_az = object_coords.transform_to(local_frame)
+celestial_object_alt_az = celestial_object_coords.transform_to(local_frame)
 
 fig = plt.figure(figsize = (12, 9))
 
-plt.plot(delta_midnight, object_alt_az.alt.degree, linewidth=2)
+plt.plot(delta_midnight, celestial_object_alt_az.alt.degree, linewidth=2)
 plt.xlabel('Local Solar Time')
 plt.ylabel('Altitude (degrees)')
 plt.xticks([-night_half_length, 0, night_half_length], ["Sunset", "Midnight", "Sunrise"])
 plt.ylim([0, 90])
 plt.yticks(np.arange(0, 91, 15))
 
-plt.savefig(object.replace(" ", "_") + "_altitude_" + month + "_" + day + "_" + year)
-# plt.show()
+plt.savefig(celestial_object.replace(" ", "_") + "_altitude_" + month + "_" + day + "_" + year)
