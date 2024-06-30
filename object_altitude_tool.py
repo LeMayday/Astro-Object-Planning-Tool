84a1e147aef3a1fadddaaceed402cd4f915eb07f
LeMayday <21mayc@gmail.com>
2024-06-30 10:08:06 -0700

    added command line arguments

--- object_altitude_tool.py
+++ object_altitude_tool.py
@@ -3,6 +3,7 @@
 # inspired by https://docs.astropy.org/en/stable/generated/examples/coordinates/plot_obs-planning.html
 # requires matplotlib, numpy, astropy, and jplephem modules
 
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
@@ -24,37 +25,46 @@ def night_half_duration(local_midnight, location):
     astro_dark_to_midnight = (n - np.argmin(np.abs(sun.alt.degree + 18))) / n * 12 if np.argmin(np.abs(sun.alt.degree + 18)) != n - 1 else 0
     return sunset_to_midnight, astro_dark_to_midnight
 
-celestial_object = input("Enter the name of the object: ")
-celestial_object_coords = SkyCoord.from_name(celestial_object)
+def plot_object(celestial_object, lat, long, date):
+    celestial_object_coords = SkyCoord.from_name(celestial_object)
+    location = EarthLocation(lat=lat * u.deg, lon=long * u.deg, height = 0 * u.m)
 
-local_coords = input("Enter your latitude and longitude in degrees (separated by a space): ")
-lat, long = local_coords.split()
-location = EarthLocation(lat=float(lat) * u.deg, lon=float(long) * u.deg, height = 0 * u.m)
+    month, day, year = date.split("/")
+    date = Time(year + "-" + month + "-" + day, format='iso', scale='utc')
+    # +1 for night of date
+    local_midnight = Time(date.to_value('jd', 'float') + 1 - long / 360, format='jd')
 
-date = input("Enter the date of your observation (mm/dd/yyyy): ")
-month, day, year = date.split("/")
-date = Time(year + "-" + month + "-" + day, format='iso', scale='utc')
-# +1 for night of date
-local_midnight = Time(date.to_value('jd', 'float') + 1 - float(long) / 360, format='jd')
+    night_half_length, astro_dark_half_length = night_half_duration(local_midnight, location)
+    if (night_half_length == 0):
+        print("Sun does not set.")
+        exit()
+    delta_night = np.linspace(-night_half_length, night_half_length, 1000) * u.hour
+    local_frame = AltAz(obstime=local_midnight + delta_night, location=location)
 
-night_half_length, astro_dark_half_length = night_half_duration(local_midnight, location)
-if (night_half_length == 0):
-    print("Sun does not set.")
-    exit()
-delta_night = np.linspace(-night_half_length, night_half_length, 1000) * u.hour
-local_frame = AltAz(obstime=local_midnight + delta_night, location=location)
+    celestial_object_alt_az = celestial_object_coords.transform_to(local_frame)
 
-celestial_object_alt_az = celestial_object_coords.transform_to(local_frame)
+    fig = plt.figure(figsize = (12, 9))
 
-fig = plt.figure(figsize = (12, 9))
+    plt.plot(delta_night, celestial_object_alt_az.alt.degree, linewidth=2)
+    plt.title(celestial_object + " on the night of " + month + "/" + day + "/" + year + " at " + str(lat) + ", " + str(long))
+    plt.xlabel('Local Solar Time')
+    plt.ylabel('Altitude (degrees)')
+    plt.xticks([-night_half_length, -astro_dark_half_length, 0, astro_dark_half_length, night_half_length], 
+            ["Sunset", "Astro Dusk", "Midnight", "Astro Dawn", "Sunrise"])
+    plt.ylim([0, 90])
+    plt.yticks(np.arange(0, 91, 15))
 
-plt.plot(delta_night, celestial_object_alt_az.alt.degree, linewidth=2)
-plt.title(celestial_object + " on the night of " + month + "/" + day + "/" + year + " at " + str(lat) + ", " + str(long))
-plt.xlabel('Local Solar Time')
-plt.ylabel('Altitude (degrees)')
-plt.xticks([-night_half_length, -astro_dark_half_length, 0, astro_dark_half_length, night_half_length], 
-           ["Sunset", "Astro Dusk", "Midnight", "Astro Dawn", "Sunrise"])
-plt.ylim([0, 90])
-plt.yticks(np.arange(0, 91, 15))
+    plt.savefig(celestial_object.replace(" ", "_") + "_altitude_" + month + "_" + day + "_" + year)
 
-plt.savefig(celestial_object.replace(" ", "_") + "_altitude_" + month + "_" + day + "_" + year)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', required=True, help="Name of object")
+    parser.add_argument('-l', '--latitude', required=True, type=float, help="Latitude as decimal degrees")
+    # parser.add_argument('-m', '--meridian', required=True, type=float, help="Longitude (meridian) as decimal degrees")
+    parser.add_argument('-d', '--date', required=True, help="Date of the observation")
+    args = parser.parse_args()
+
+    plot_object(args.name, args.parallel, 0, args.date)
+
+if __name__ == "__main__":
+    main()
